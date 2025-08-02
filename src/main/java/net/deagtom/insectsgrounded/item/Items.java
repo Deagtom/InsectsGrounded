b7aed2a4f8cc9684a11b94babf976656943df086
Dagir <dagir.ram@mail.ru>
2025-08-03 00:18:52 +0300

    Grounded in Minecraft

---
+++ src/main/java/net/deagtom/insectsgrounded/item/Items.java
@@ -0,0 +1,17 @@
+package net.deagtom.insectsgrounded.item;
+
+import net.deagtom.insectsgrounded.InsectsGrounded;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class Items {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(InsectsGrounded.MODID);
+
+    public static final DeferredItem<Item> PEBBLET = ITEMS.register("pebblet", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
