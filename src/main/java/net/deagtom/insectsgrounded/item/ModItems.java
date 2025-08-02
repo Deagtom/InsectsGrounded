522ddcbb4335584519bd6ec2e4e2667d18b5646c
Dagir <dagir.ram@mail.ru>
2025-08-03 00:57:36 +0300

    First items from grounded

--- src/main/java/net/deagtom/insectsgrounded/item/ModItems.java
+++ src/main/java/net/deagtom/insectsgrounded/item/ModItems.java
@@ -6,10 +6,11 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-public class Items {
+public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(InsectsGrounded.MODID);
 
     public static final DeferredItem<Item> PEBBLET = ITEMS.register("pebblet", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> SPRIG = ITEMS.register("sprig", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
